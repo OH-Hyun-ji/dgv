c963c155547978e0165198c4f88d5fb512aa22e0
윤호영 <ghdud1122@gmail.com>
2022-02-26 02:25:33 +0900

    누나 오늘 고생하셨습니다.

--- src/main/java/com/dgv/web/user/controller/UserLoginController.java
+++ src/main/java/com/dgv/web/user/controller/UserLoginController.java
@@ -1,22 +1,32 @@
 package com.dgv.web.user.controller;
 
+import static org.junit.Assert.assertThat;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dgv.web.user.service.UserService;
 import com.dgv.web.user.vo.UserVO;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @Controller
 public class UserLoginController {
 
 	private final UserService userService;
+
 
 	// @RequestMapping("/login.do")
 //	public String login() {
@@ -34,18 +44,37 @@ public class UserLoginController {
 	}
 
 	// 로그인 처리
-	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
-	public String loginPOST(UserVO userVO, HttpSession httpSession, Model model) throws Exception {
-		UserVO userVo = userService.login(userVO);
-		if (userVo == null || !BCrypt.checkpw(userVO.getUser_pw(), userVo.getUser_pw())) {
+	@PostMapping("/login.do")
+	@ResponseBody
+	public String loginPOST(@RequestBody UserVO userVO, HttpSession session, Model model) {
+		System.out.println("id : "+ userVO.getUser_id());
+		System.out.println("pw : "+ userVO.getUser_pw());
+		UserVO userId = userService.login(userVO);
+		
+		System.out.println(BCrypt.checkpw(userVO.getUser_pw(), userId.getUser_pw()));
+		System.out.println("TEST 1 : "+userId.getUser_pw() );
+		Gson gson = new Gson();
+		JsonObject jsonObject = new JsonObject();
+		if(userVO.getUser_id().equals(userId.getUser_id())&&BCrypt.checkpw(userVO.getUser_pw(), userId.getUser_pw())) {
+			System.out.println("로그인 성공!!");
+			jsonObject.addProperty("msg", "SUCCESS");
+			session.setAttribute("userID", userId.getUser_name());
+			System.out.println(jsonObject);		
+		}else { 
 			System.out.println("로그인 실패");
-			return "/login/user_loginForm";
-		} else {
-			model.addAttribute("user", userVo);
-			model.addAttribute("userId", userVO.getUser_id());
-			model.addAttribute("userName", userVo.getUser_name());
-			System.out.println("로그인 성공");
-			 return "/login/user_login";
+			jsonObject.addProperty("msg", "FAIL");
+			System.out.println(jsonObject);		
 		}
+		String jsonResult = gson.toJson(jsonObject);
+		
+		return jsonResult;
+ 	}
+	
+	// 로그아웃 처리
+	@RequestMapping("/logout.do")
+	public String userLogout(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		return"redirect:loginForm.do";
 	}
 }
